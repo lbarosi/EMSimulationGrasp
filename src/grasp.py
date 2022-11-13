fbbd8f565bf16513a5cb40c269bdaf1f19e38afc
lbarosi <lbarosi@df.ufcg.edu.br>
2022-11-13 13:05:12 -0300

    metrics implemented.

--- src/grasp.py
+++ src/grasp.py
@@ -9,6 +9,7 @@ import subprocess
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+import scipy as sp
 
 #######################################
 # Funções para manipulação de cuts
@@ -29,11 +30,17 @@ def read_cut(filename):
     data["theta"] = thetas
     return data
 
+
 def load_cuts(mask):
     cut_files = glob.glob(mask, recursive=True)
     dfs = []
     gains = []
+    eta_spill = []
+    eta_pol = []
+    eta_amp = []
+    eta_phase = []
     gains_max = []
+    FWHMs = []
     params = []
     for file in cut_files:
         df = read_cut(file)
@@ -45,11 +52,23 @@ def load_cuts(mask):
             param = 0.
         else:
             param = param[0]
+        df = center_beam(df)
         params.append(param)
-        gains.append(gain_dB(df))
-        gains_max.append(gain_max(df))
-    df_gains = pd.DataFrame({"Values": params, "Gain": gains,
-                            "Gain_Max": gains_max}).sort_values(by="Values")
+        FWHM = get_FWHM(df)
+        FWHMs.append(FWHM)
+        gains.append(gain(df, FWHM))
+        eta_spill.append(n_spill(df, FWHM))
+        eta_pol.append(n_pol(df, FWHM))
+        eta_phase.append(n_phase(df, FWHM))
+        eta_amp.append(n_amp(df, FWHM))
+        gains_max.append(gain_max(df, FWHM))
+    df_gains = pd.DataFrame({"Values": params, "FWHM": FWHMs,
+                             "Gain": gains,
+                             "eta_spill": eta_spill,
+                             "eta_pol": eta_pol,
+                             "eta_amp": eta_amp,
+                             "eta_phase": eta_phase,
+                             "Gain_Max": gains_max})
     return df_gains, dfs
 
 def plot_beam_pattern(data, ax=None, label="padrão", norm=True):
@@ -77,88 +96,131 @@ def plot_beam_pattern(data, ax=None, label="padrão", norm=True):
     #ax.plot(data.theta, 10 * np.log10(EEx))
     ax.grid(axis="y")
     #ax.set_ylim([-100, 0])
-    ax.set_xlabel("thetas")
+    ax.set_xlabel(r"$\theta (\^\circ)$")
     ax.set_ylabel("Amplitude (dB)")
     ax.legend()
     return ax
 
 #######################################
-# Métricas para ganho da antena.
+# Utilidades para padrão de radiação
 #######################################
 
 
-def I1(data):
+def get_B(data, type="co"):
+    Bco = data.Eco ** 2 + data.ImEco ** 2
+    Bcx = data.Ecx ** 2 + data.ImEcx ** 2
+    BB = Bco + Bcx
+    if type == "co":
+        result = Bco
+    elif type == "cx":
+        result = Bcx
+    elif type == "total":
+        result = BB
+    return result
+
+
+def _interpolate_beam(x, data):
+    BB = get_B(data, type="total")
+    result = sp.interpolate.interp1d(data.theta, BB)
+    return -result(x)
+
+
+def get_FWHM(data, theta0=.1):
+    BB = get_B(data, type="total")
+    FWHM_func = lambda theta: _interpolate_beam(theta, data) +\
+        BB.max() / 2
+    FWHM = sp.optimize.root(FWHM_func, theta0, method="lm").x[0]
+    return FWHM
+
+#######################################
+# Métricas para ganho da antena.
+#######################################
+def center_beam(data):
+    df = data.copy()
+    BB = get_B(df, type="total")
+    idx = BB.argmax()
+    theta_c = df.iloc[idx].theta
+    df.theta = df.theta - theta_c
+    return df
+
+def I1(data, theta_max):
     # ref Pontoppidan
-    Eco2 = data.Eco ** 2 + data.ImEco ** 2
-    Ecx2 = data.Ecx ** 2 + data.ImEcx ** 2
-    EE2 = Eco2 + Ecx2
-    I1 = 2 * np.pi * np.trapz(EE2 * np.sin(data.theta), data.theta)
+    df = data.copy()
+    df = df[(df.theta >= 0) & (df.theta <= theta_max)]
+    BB = get_B(df, type="total")
+    I1 = 2 * np.pi * np.trapz(BB * np.sin(np.radians(df.theta)),
+                              np.radians(df.theta))
     return I1
 
 
-def I2(data):
+def I2(data, theta_max):
     # ref Pontoppidan
-    Eco2 = data.Eco ** 2 + data.ImEco ** 2
-    I2 = 2 * np.pi * np.trapz(Eco2 * np.sin(data.theta), data.theta)
+    df = data.copy()
+    df = df[(df.theta >= 0) & (df.theta <= theta_max)]
+    BBco = get_B(df, type="co")
+    I2 = 2 * np.pi * np.trapz(BBco * np.sin(np.radians(df.theta)),
+                              np.radians(df.theta))
     return I2
 
 
-def I3(data):
+def I3(data, theta_max):
     # ref Pontoppidan
-    Eco = np.sqrt(data.Eco ** 2 + data.ImEco ** 2)
-    I3 = 2 * np.pi * np.trapz(Eco * np.sin(data.theta), data.theta)
+    df = data.copy()
+    df = df[(df.theta >= 0) & (df.theta <= theta_max)]
+    EEco = np.sqrt(get_B(df, type="co"))
+    I3 = 2 * np.pi * np.trapz(EEco * np.sin(np.radians(df.theta)),
+                              np.radians(df.theta))
     return I3
 
 
-def I4(data):
+def I4(data, theta_max):
     # ref Pontoppidan
-    ReI4 = np.trapz(data.Eco * np.sin(data.theta), data.theta)
-    ImI4 = np.trapz(data.ImEco * np.sin(data.theta), data.theta)
-    I4 = 2 * np.pi * np.sqrt(ReI4 ** 2 + ImI4 ** 2)
+    df = data.copy()
+    df = df[(df.theta >= 0) & (df.theta <= theta_max)]
+    ReI4 = 2 * np.pi * np.trapz(df.Eco * np.sin(np.radians(df.theta)),
+                                np.radians(df.theta))
+    ImI4 = 2 * np.pi * np.trapz(df.ImEco * np.sin(np.radians(df.theta)),
+                                np.radians(df.theta))
+    I4 = np.sqrt(ReI4 ** 2 + ImI4 ** 2)
     return I4
 
 
-def n_spill(data):
+def n_spill(data, FWHM):
     # ref Pontoppidan
-    result = I1(data) / 4 * np.pi
+    result = I1(data, FWHM) / I1(data, np.pi)
     return result
 
 
-def n_pol(data):
+def n_pol(data, FWHM):
     # ref Pontoppidan
-    result = I2(data) / I1(data)
+    result = I2(data, FWHM) / I1(data, FWHM)
     return result
 
 
-def n_amp(data):
+def n_amp(data, FWHM):
     # ref Pontoppidan
-    result = I3(data) ** 2 / I2(data)
+    Omega = 2 * np.pi * (1 - np.cos(np.radians(FWHM)))
+    result = I3(data, FWHM) ** 2 / (I2(data, FWHM) * Omega)
     return result
 
 
-def n_phase(data):
+def n_phase(data, FWHM):
     # ref Pontoppidan
-    result = I4(data) ** 2 / I3(data)
+    result = I4(data, FWHM) ** 2 / I3(data, FWHM) ** 2
     return result
 
 
-def gain_dB(data):
+def gain(data, FWHM):
     # ref Pontoppidan
-    result = 10 * np.log10(I4(data) ** 2)
+    Omega = 2 * np.pi * (1 - np.cos(np.radians(FWHM)))
+    result = I4(data, FWHM) ** 2 / (I1(data, np.pi) * Omega)
     return result
 
 
-def gain_dB_O(data):
-    # ref Olmi
-    result = 10 * np.log10(I4(data) ** 2 / I2(data))
-    return result
-
-
-def gain_max(data):
+def gain_max(data, FWHM):
     # ref Gan
-    idx = (data.Eco**2 + data.ImEco**2 + data.Ecx**2 + data.ImEcx**2).argmax()
-    Bmax = data.iloc[idx].Eco ** 2 + data.iloc[idx].ImEco ** 2 + data.iloc[idx].Ecx ** 2 + data.iloc[idx].ImEcx ** 2
-    result = 10 * np.log10(2 * Bmax / I1(data))
+    BB = get_B(data, type="total")
+    result = 10 * np.log10(4 * np.pi * BB.max() / I1(data, np.pi))
     return result
 
 #######################################
@@ -212,16 +274,16 @@ def run_grasp(tor_file, gpxfile="../grasp/STANDARD/batch.gxp", tcifile="../grasp
     return result
 
 
-def move_feed(x=0, y=0, z=0, theta=0, phi=0):
+def move_feed(x=0., y=0., z=0., theta=0., phi=0.):
     x0 = x
     y0 = y
     z0 = z
-    xx = np.cos(np.radians(theta))
-    xy = np.sin(np.radians(theta)) * np.sin(np.radians(phi))
-    xz = np.sin(np.radians(theta)) * np.cos(np.radians(phi))
+    xx = np.round(np.cos(np.radians(theta)), 4)
+    xy = np.round(np.sin(np.radians(theta)) * np.sin(np.radians(phi)), 4)
+    xz = np.round(np.sin(np.radians(theta)) * np.cos(np.radians(phi)), 4)
     yx = 0.
-    yy = np.cos(np.radians(phi))
-    yz = -np.sin(np.radians(phi))
+    yy = np.round(np.cos(np.radians(phi)), 4)
+    yz = np.round(-np.sin(np.radians(phi)), 4)
     dict = "corneta_coor  coor_sys\n(\n  origin           : struct(x: " + \
         str(x0) + " m, y: " + str(y0) + " m, z: " + str(z0) + " m),\n" + \
         "  x_axis           : struct(x: " +\
@@ -269,9 +331,10 @@ def _make_tor(filename, string, torfile, idx_i, idx_f):
 
 
 def make_tor(object="feed", X0=0, Y0=0, Z0=0, theta=0, phi=0,
-             torfile="../grasp/STANDARD/BINGO_CUT.tor", verbose=True):
-    dirname = "../data/raw/grasp/job" + "_" + object + "_" + str(X0) + "_" +\
-        str(Y0) + "_" + str(Z0) + "_" + str(theta) + "_" + str(phi) + "/"
+             torfile="../grasp/STANDARD/BINGO_CUT.tor", verbose=True,
+             prefix="grasp"):
+    dirname = "../data/raw/grasp/job" + prefix + "_" + object + "_" + str(X0) +\
+        "_" + str(Y0) + "_" + str(Z0) + "_" + str(theta) + "_" + str(phi) + "/"
     # cria pasta
     if not os.path.isdir(dirname):
         os.mkdir(dirname)
